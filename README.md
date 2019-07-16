23479cda4a1a6b24483a060cb5278911e4c2d12b
Wynn Chaney <wynn.chaney@wanco.com>
2019-07-16 11:06:58 -0600

    Added functionality of bamazon prompts and mysql queries, added text to readme.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bamazon
+
+Bamazon is a mock database that prompts the user for selections. The selection requirements are as follows:
+
+1. User enters the id of the item they would like to purchase.
+2. Once the id is verified as a valid id the user is prompted for the quantity to purchase.
+3. Once the quantity to purchase is verified to exists the transaction is completed or returns 'Insufficient quantity!' if it is an invalid quantity.
+
+The technologies uses are node.js with mysql and inquirer and javascript.
--- javascript/bamazonCustomer.js
+++ javascript/bamazonCustomer.js
@@ -2,24 +2,116 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "bamazonDB"
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "bamazonDB"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
+  if (err) throw err;
+  runInventory();
+});
+
+
+function runInventory() {
+  var query = "SELECT * FROM products";
+  connection.query(query, function (err, res) {
     if (err) throw err;
-    runSearch();
-});
 
-function runSearch() {
-    var query = "SELECT item_id, product_name, department_name, price FROM products";
-    connection.query(query, function(err, res) {
-        if (err) throw err;
+    console.log(res);
+    promptUser(res);
+
+  })
+
+  // connection.end();
+
+}
+
+// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+
+// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+//    * This means updating the SQL database to reflect the remaining quantity.
+//    * Once the update goes through, show the customer the total cost of their purchase.
+
+function promptUser(inventory) {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "userChoice",
+      message: "What is the ID of the item you would you like to purchase? (^C to exit)",
+      validate: function (val) {
+        return !isNaN(val) || val.toLowerCase() === 'e';
+      }
+    }
+  ])
+    .then(function (val) {
+
+      var userChoiceId = parseInt(val.userChoice);
+      // verify id is valid
+      var stock = checkInventory(userChoiceId, inventory)
+      if (stock) {
+        promptQuantity(stock)
+      } else {
+        console.log("Please select a valid item.");
+        runInventory();
+      }
+
+
+    });
+}
+
+function promptQuantity(stock) {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "quantity",
+      message: "How many do you want?",
+      validate: function (val) {
+        return !isNaN(val);
+      }
+    }
+  ])
+    .then(function (val) {
+      var quantity = parseInt(val.quantity);
+      
+      if (quantity > stock.stock_quantity) {
+        console.log("\nInsufficient quantity!");
+        runInventory();
+      }
+      else {
+       
+        // console.log(stock);
+        makeTransaction(stock, quantity)
         
-            console.log(res);
-        
+      }
     })
+}
+
+function checkInventory(userChoiceId, inventory) {
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].item_id === userChoiceId) {
+     
+      return inventory[i];
+    }
+  }
+  
+  return null;
+}
+
+function makeTransaction(stock, quantity) {
+  
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, stock.item_id],
+    function(err, res) {
+      
+      console.log(`Successfully purchased ${quantity} of ${stock.product_name}.`)
+      
+      runInventory();
+    }
+  );
 }
